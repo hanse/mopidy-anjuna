5dbd8dc83318ba961bfe58d0ffc26451f10fde4b
Hanse <hanse19@gmail.com>
2014-10-24 16:19:23 +0200

    Add table sorting capabilities

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -4,48 +4,53 @@ var React = require('react');
 
 var SongList = React.createClass({
 
-  render: function() {
-    var songs = [
-      {name: 'Love Never Felt So Good', artist: 'Michael Jackson, Justin Timberlake'},
-      {name: 'Love Never Felt So Good', artist: 'Michael Jackson, Justin Timberlake'},
-      {name: 'Love Never Felt So Good', artist: 'Michael Jackson, Justin Timberlake'},
-      {name: 'Love Never Felt So Good', artist: 'Michael Jackson, Justin Timberlake'},
-      {name: 'Love Never Felt So Good', artist: 'Michael Jackson, Justin Timberlake'},
-      {name: 'Love Never Felt So Good', artist: 'Michael Jackson, Justin Timberlake'},
-      {name: 'Love Never Felt So Good', artist: 'Michael Jackson, Justin Timberlake'},
-      {name: 'Love Never Felt So Good', artist: 'Michael Jackson, Justin Timberlake'},
-      {name: 'Love Never Felt So Good', artist: 'Michael Jackson, Justin Timberlake'},
-      {name: 'Love Never Felt So Good', artist: 'Michael Jackson, Justin Timberlake'},
-      {name: 'Love Never Felt So Good', artist: 'Michael Jackson, Justin Timberlake'},
-      {name: 'Love Never Felt So Good', artist: 'Michael Jackson, Justin Timberlake'},
-      {name: 'Love Never Felt So Good', artist: 'Michael Jackson, Justin Timberlake'},
-      {name: 'Love Never Felt So Good', artist: 'Michael Jackson, Justin Timberlake'},
-      {name: 'Love Never Felt So Good', artist: 'Michael Jackson, Justin Timberlake'},
-      {name: 'Love Never Felt So Good', artist: 'Michael Jackson, Justin Timberlake'},
-      {name: 'Love Never Felt So Good', artist: 'Michael Jackson, Justin Timberlake'},
-      {name: 'Love Never Felt So Good', artist: 'Michael Jackson, Justin Timberlake'},
-      {name: 'Love Never Felt So Good', artist: 'Michael Jackson, Justin Timberlake'},
-      {name: 'Love Never Felt So Good', artist: 'Michael Jackson, Justin Timberlake'},
-      {name: 'Love Never Felt So Good', artist: 'Michael Jackson, Justin Timberlake'},
-      {name: 'Love Never Felt So Good', artist: 'Michael Jackson, Justin Timberlake'},
-    ];
+  getInitialState: function() {
+    return {
+      songs: [
+        {name: 'Love Never Felt So Good', artist: 'Michael Jackson, Justin Timberlake'},
+        {name: 'Titanium (feat. Sia)', artist: 'David Guetta, Sia'},
+        {name: 'Sleepless - Radio Edit', artist: 'CAZZETTE, The High'},
+        {name: 'Pompeii', artist: 'Bastille'},
+      ],
+      sortDirection: 1,
+    };
+  },
 
+  _onSort: function(field) {
+    var prev = this.state.songs;
+    var direction = this.state.sortDirection;
+    this.setState({
+      songs: prev.sort(function(a, b) {
+        if (!a.hasOwnProperty(field)) throw new Error(`The object does not have a property named "${field}" to sort on.`);
+        return direction * a[field].localeCompare(b[field]);
+      }),
+      sortDirection: direction * -1
+    });
+  },
+
+  render: function() {
+    console.log(this.state)
+    var songs = this.state.songs;
     return (
       <table>
-      <tr>
-        <th>Track</th>
-        <th>Artist</th>
-        <th>Time</th>
-      </tr>
-      {songs.map(function(song) {
-        return (
+        <thead>
           <tr>
-            <td>{song.name}</td>
-            <td>{song.artist}</td>
-            <td>1:05</td>
+            <th onClick={this._onSort.bind(this, 'name')}>Track</th>
+            <th onClick={this._onSort.bind(this, 'artist')}>Artist</th>
+            <th>Time</th>
           </tr>
-        );
-      })}
+        </thead>
+      <tbody>
+        {songs.map(function(song) {
+          return (
+            <tr>
+              <td>{song.name}</td>
+              <td>{song.artist}</td>
+              <td>1:05</td>
+            </tr>
+          );
+        })}
+      </tbody>
       </table>
     );
   }
