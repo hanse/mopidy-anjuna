45219c28f93e04aede71f860963a040aeccb4326
Hanse <hanse19@gmail.com>
2015-06-26 19:10:08 +0200

    Add loading help text

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -8,10 +8,21 @@ class Loader extends React.Component {
 
   static defaultProps = { loading: false }
 
+  state = { excessiveLoading: false }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ excessiveLoading: true })
+    }, 2000);
+  }
+
   render() {
     if (this.props.loading) {
       return (
-        <div className='spinner'></div>
+        <div className='Loader'>
+          <div className='spinner'></div>
+          <p>{this.state.excessiveLoading ? 'Not loading? Make sure mopidy is running.' : ''}</p>
+        </div>
       );
     }
     return this.props.children;
